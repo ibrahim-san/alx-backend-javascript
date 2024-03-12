d2b26e79922833559adb58f60e4facbda68ccdf7
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-03-12 11:38:26 +0100

    task 3 attempted

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 const getStudentsByLocation = (students, city) => {
-    const arrayObjec = students.filter((func) => func.location === city);
+    const arrayObjec = students.filter((dic) => dic.location === city);
     return arrayObjec;
   };
   
-  export default getStudentsByLocation;
+export default getStudentsByLocation;
