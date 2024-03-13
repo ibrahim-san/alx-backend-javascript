11bd0d0766dc2e0d8c73028095112498522286d3
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-03-13 13:58:01 +0100

    task 3 attempted

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,23 @@ const teacher3: Teacher = {
   };
   
   console.log(teacher3);
+
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+
+  console.log(director1);
+
+  function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+  }
+
+  console.log(printTeacher("John", "Doe"));
