91c292e2ec59ba98a2378f6b93e5d61a7df81944
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-03-13 15:05:27 +0100

    task 7 attempted

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,84 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string) : Teacher | Director {
+    if(typeof(salary) == "number" && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+console.log(createEmployee(200));
+
+console.log(createEmployee(1000));
+
+console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher) {
+    if(isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    } else if (todayClass === "History") {
+        return "Teaching History";
+    } else {
+        throw new Error("Invalid subject");
+    }
+}
+
+console.log(teachClass("Math"));
+
+console.log(teachClass("History"));
