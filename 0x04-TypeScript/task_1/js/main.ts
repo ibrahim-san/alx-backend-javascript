1a14540e3eb1eb0a6ad63b6368dfd6636c5e95f5
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-03-13 14:48:21 +0100

    task 6 attempted

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,26 +13,55 @@ const teacher3: Teacher = {
     lastName: 'Doe',
     location: 'London',
     contract: false,
-  };
-  
-  console.log(teacher3);
+};
 
-  interface Directors extends Teacher {
+console.log(teacher3);
+
+interface Directors extends Teacher {
     numberOfReports: number;
-  }
+}
 
-  const director1: Directors = {
+const director1: Directors = {
     firstName: 'John',
     lastName: 'Doe',
     location: 'London',
     fullTimeEmployee: true,
     numberOfReports: 17,
-  };
+};
 
-  console.log(director1);
+console.log(director1);
 
-  function printTeacher(firstName: string, lastName: string): string {
+function printTeacher(firstName: string, lastName: string): string {
     return `${firstName[0]}. ${lastName}`;
-  }
+}
 
-  console.log(printTeacher("John", "Doe"));
+console.log(printTeacher("John", "Doe"));
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public workOnHomework(): string {
+        return "Currently Working";
+    }
+
+    public displayName(): string {
+        return this.firstName;
+    }
+}
